b077db17716a47e1babfab0365641e1078297abe
sumath <killesumath.chandra@hcl.com>
2021-12-12 15:53:10 +0530

    first commit

---
+++ src/main/java/com/devops/assign/Week10GradedApplication.java
@@ -0,0 +1,14 @@
+package com.devops.assign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10GradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10GradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ src/test/java/com/devops/assign/Week10GradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.assign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10GradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
